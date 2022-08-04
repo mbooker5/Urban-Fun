de71aba0f382ab390530d465b216ee16ccbeb1b7
mbooker5 <102274742+mbooker5@users.noreply.github.com>
2022-08-03 18:38:10 -0700

    Revert "Revert "Search features""

--- Urban-Fun/Helpers/HelperClass.h
+++ Urban-Fun/Helpers/HelperClass.h
@@ -7,11 +7,28 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import "Activity.h"
+#import "User.h"
 
 NS_ASSUME_NONNULL_BEGIN
+static NSString *const kDistance = @"distance";
+static NSString *const kDistanceSliderValue = @"distanceSliderValue";
+static NSString *const kCategories = @"categories";
+static NSString *const kCategoriesCount = @"categoriesCount";
+static NSString *const kMinimumAge = @"minimumAge";
+static NSString *const kMaximumAge = @"maximumAge";
+static NSString *const kAvailability = @"availability";
 
 @interface HelperClass : NSObject
 + (void)showAlertWithTitle:(NSString *)alertTitle withMessage:(NSString *)alertMessage withActionTitle:(NSString *)actionTitle withHandler:(nullable SEL)func onVC:(UIViewController *)vc;
+
++ (void) activityQuerywithText:(NSString *)searchText useFilters:(NSMutableDictionary *)filtersDictionary withCompletion:(void(^)(NSArray *activities))completion;
+
++ (void) userQuerywithText:(NSString *)searchText withCompletion:(void(^)(NSArray *users))completion;
+
++ (NSNumber *) distanceFromUserLocation:(CLLocation *)userLocation forActivity:(Activity *)activity;
+
++ (CLLocation *)getCLLocationForGeoPoint:(PFGeoPoint *)location;
 @end
 
 NS_ASSUME_NONNULL_END
